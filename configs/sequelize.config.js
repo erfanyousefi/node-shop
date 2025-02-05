b80010c9d248d709f02ac5b5b80b91740733ff41
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-06 02:11:49 +0330

    add my local configs fir connected to mariadb

--- configs/sequelize.config.js
+++ configs/sequelize.config.js
@@ -1,15 +1,23 @@
 const {Sequelize} = require("sequelize");
 
+// const sequelize = new Sequelize({
+//     dialect: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "phpmyadmin",
+//     password: "root",
+//     database: "shop"
+// });
 const sequelize = new Sequelize({
-    dialect: "mysql",
+    dialect: "mariadb",
     host: "localhost",
     port: 3306,
-    username: "phpmyadmin",
+    username: "root",
     password: "root",
     database: "shop"
 });
 sequelize.authenticate().then(async () => {
-    await sequelize.sync({alter: true});
+    // await sequelize.sync({alter: true});
     await sequelize.sync({alter: true});
     console.log("connected to MySQL");
 }).catch(err => {
