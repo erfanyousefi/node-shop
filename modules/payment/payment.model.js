396e04a145600583ba3a61e34cdf056fbf8a8f38
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-06 02:10:07 +0330

    fix payment timestamps and remove payment relations in payment model file

--- modules/payment/payment.model.js
+++ modules/payment/payment.model.js
@@ -1,30 +1,16 @@
 const {DataTypes} = require("sequelize");
 const sequelize = require("../../configs/sequelize.config");
-const {Order} = require("../order/order.model");
-
 const Payment = sequelize.define("payment", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     status: {type: DataTypes.BOOLEAN, defaultValue: false},
     amount: {type: DataTypes.DECIMAL},
     refId: {type: DataTypes.STRING, allowNull: true},
     authority: {type: DataTypes.STRING, allowNull: true},
-    orderId: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        unique: true,
-        references: {
-            model: Order,
-            key: "id"
-        }
-    }
-}, {createdAt: "created_at"});
-Payment.belongsTo(Order, {
-    foreignKey: "orderId",         // Foreign key in the Payment table
-    targetKey: "id",               // Primary key in the Order table
-    as: "order",                   // Alias for querying
-    onDelete: "SET NULL"           // Set orderId to NULL if the Order is deleted
-});
-
+    orderId: {type: DataTypes.INTEGER, allowNull: true, unique: true}
+}, {createdAt: "created_at", updatedAt: false});
+// Order.hasOne(Payment, {foreignKey: "paymentId", as: "payment", sourceKey: "id"});
+// Payment.belongsTo(Order, {foreignKey: "orderId", as: "order", targetKey: "id"});
+// Payment.sync();
 module.exports = {
     Payment
 };
