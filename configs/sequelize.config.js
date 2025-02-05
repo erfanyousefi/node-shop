5e8570b9ec750e1edff6a9c52835128372a00727
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-05 17:11:22 +0330

    sequelize connection  config file

---
+++ configs/sequelize.config.js
@@ -0,0 +1,18 @@
+const {Sequelize} = require("sequelize");
+
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "phpmyadmin",
+    password: "root",
+    database: "shop"
+});
+sequelize.authenticate().then(async () => {
+    await sequelize.sync({alter: true});
+    await sequelize.sync({alter: true});
+    console.log("connected to MySQL");
+}).catch(err => {
+    console.log("Cannot connect to MySQL, error: ", err?.message);
+});
+module.exports = sequelize; 
