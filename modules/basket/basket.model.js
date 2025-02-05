398b975dcffa80f66dcdc0c98769865525f375c7
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-06 02:12:18 +0330

    remove all relations in basket model file

--- modules/basket/basket.model.js
+++ modules/basket/basket.model.js
@@ -1,9 +1,5 @@
 const {DataTypes} = require("sequelize");
 const sequelize = require("../../configs/sequelize.config");
-const {User} = require("../user/user.model");
-const {Product, ProductColor, ProductSize} = require("../product/product.model");
-const {Discount} = require("../discount/discout.model");
-
 const Basket = sequelize.define("basket", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     userId: {type: DataTypes.INTEGER},
@@ -12,23 +8,7 @@ const Basket = sequelize.define("basket", {
     colorId: {type: DataTypes.INTEGER, allowNull: true},
     discountId: {type: DataTypes.INTEGER, allowNull: true},
     count: {type: DataTypes.INTEGER},
-});
-
-Basket.hasMany(User, {foreignKey: "userId"});
-User.belongsTo(Basket, {onDelete: "CASCADE"});
-
-Basket.hasMany(Product, {foreignKey: "productId"});
-Product.belongsTo(Basket, {onDelete: "CASCADE"});
-
-Basket.hasMany(ProductColor, {foreignKey: "colorId"});
-ProductColor.belongsTo(Basket, {onDelete: "CASCADE"});
-
-Basket.hasMany(ProductSize, {foreignKey: "sizeId"});
-ProductSize.belongsTo(Basket, {onDelete: "CASCADE"});
-
-Basket.hasMany(Discount, {foreignKey: "discountId"});
-Discount.belongsTo(Basket, {onDelete: "CASCADE"});
-
+}, {timestamps: false});
 module.exports = {
     Basket
 };
