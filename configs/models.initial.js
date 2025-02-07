ab84fa08de2fbfb025d672aca9e98f395d8071e4
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-08 01:40:13 +0330

    change models relations

--- configs/models.initial.js
+++ configs/models.initial.js
@@ -6,37 +6,38 @@ const {Product, ProductDetail, ProductSize, ProductColor} = require("../modules/
 const {User, Otp} = require("../modules/user/user.model");
 const sequelize = require("./sequelize.config");
 
-async function initDatabase (options = {}) {
-    User.hasOne(Otp, {as: "otp", foreignKey: "userId"});
-    Otp.hasOne(User, {as: "user", foreignKey: "otpId", onDelete: "SET NULL"});
-    User.hasMany(Order, {foreignKey: "userId", sourceKey: "id", as: "orders"});
-    Order.hasMany(OrderItems, {foreignKey: 'orderId', as: 'items', sourceKey: "id"});
-    OrderItems.belongsTo(Order, {foreignKey: 'orderId', as: 'order', targetKey: "id", onDelete: "CASCADE"});
+async function initDatabase () {
+    User.hasOne(Otp, {foreignKey: "userId", as: "otp"});
+    Otp.belongsTo(User, {foreignKey: "userId"});
+    Otp.hasOne(User, {foreignKey: "otpId", onDelete: "SET NULL"});
+    User.hasMany(Order, {foreignKey: "userId", sourceKey: "id"});
+    Order.hasMany(OrderItems, {foreignKey: 'orderId', sourceKey: "id"});
+    OrderItems.belongsTo(Order, {foreignKey: 'orderId', targetKey: "id", onDelete: "CASCADE"});
 
-    Order.hasOne(Payment, {foreignKey: "orderId", as: "payment", sourceKey: "id"});
-    Payment.hasOne(Order, {foreignKey: "paymentId", as: "order", sourceKey: "id"});
+    Order.hasOne(Payment, {foreignKey: "orderId"});
+    Payment.hasOne(Order, {foreignKey: "paymentId"});
 
-    Product.hasMany(ProductDetail, {as: "details"});
-    Product.hasMany(Discount, {as: "discounts", foreignKey: "productId", onDelete: "CASCADE"});
-    Product.hasMany(ProductSize, {as: "sizes"});
-    Product.hasMany(ProductColor, {as: "colors"});
-    ProductDetail.belongsTo(Product, {foreignKey: "productId", as: "product", targetKey: "id"});
+    Product.hasMany(ProductDetail, {foreignKey: "productId", sourceKey: "id", as: "details"});
+    Product.hasMany(Discount, {foreignKey: "productId", sourceKey: "id", as: "discounts"});
+    Product.hasMany(ProductSize, {foreignKey: "productId", sourceKey: "id", as: "sizes"});
+    Product.hasMany(ProductColor, {foreignKey: "productId", sourceKey: "id", as: "colors"});
+    ProductDetail.belongsTo(Product, {foreignKey: "productId", targetKey: "id"});
     ProductSize.belongsTo(Product, {foreignKey: "productId", targetKey: "id"});
     ProductColor.belongsTo(Product, {foreignKey: "productId", targetKey: "id"});
 
 
-    Product.hasMany(Basket, {foreignKey: "productId", as: "baskets", onDelete: "CASCADE"});
-    ProductColor.hasMany(Basket, {foreignKey: "colorId", as: "baskets", onDelete: "CASCADE"});
-    ProductSize.hasMany(Basket, {foreignKey: "sizeId", as: "baskets", onDelete: "CASCADE"});
-    Discount.hasMany(Basket, {foreignKey: "discountId", as: "baskets", onDelete: "CASCADE"});
+    Product.hasMany(Basket, {foreignKey: "productId", sourceKey: "id", as: "baskets"});
+    ProductColor.hasMany(Basket, {foreignKey: "colorId", sourceKey: "id", as: "baskets"});
+    ProductSize.hasMany(Basket, {foreignKey: "sizeId", sourceKey: "id", as: "baskets"});
+    Discount.hasMany(Basket, {foreignKey: "discountId", sourceKey: "id", as: "baskets"});
+    User.hasMany(Basket, {foreignKey: "userId", sourceKey: "id", as: "baskets"});
 
-    User.hasMany(Basket, {foreignKey: "userId", sourceKey: "id", as: "baskets", onDelete: "CASCADE"});
-    Basket.belongsTo(Discount, {as: "discount", foreignKey: "discountId"});
-    Basket.belongsTo(Product, {as: "product", foreignKey: "productId"});
-    Basket.belongsTo(ProductColor, {as: "color", foreignKey: "colorId"});
-    Basket.belongsTo(ProductSize, {as: "size", foreignKey: "sizeId"});
-    Basket.belongsTo(User, {as: "user", foreignKey: "userId"});
+    Basket.belongsTo(Discount, {foreignKey: "discountId", targetKey: "id", as: "discount"});
+    Basket.belongsTo(Product, {foreignKey: "productId", targetKey: "id", as: "product"});
+    Basket.belongsTo(ProductColor, {foreignKey: "colorId", targetKey: "id", as: "color"});
+    Basket.belongsTo(ProductSize, {foreignKey: "sizeId", targetKey: "id", as: "size"});
+    Basket.belongsTo(User, {foreignKey: "userId", targetKey: "id", as: "user"});
 
-    await sequelize.sync({force: true});
+    // await sequelize.sync({});
 }
 module.exports = initDatabase;
