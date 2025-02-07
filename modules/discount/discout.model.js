3830a3049e234a40337fb6a730faae8ca987b748
Erfan Yousefi <erfanyousefi.co@gmail.com>
2025-02-08 01:41:08 +0330

    change indexing in otp, discount

--- modules/discount/discout.model.js
+++ modules/discount/discout.model.js
@@ -2,7 +2,7 @@ const {DataTypes} = require("sequelize");
 const sequelize = require("../../configs/sequelize.config");
 const Discount = sequelize.define("discount", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    code: {type: DataTypes.STRING(50), unique: true},
+    code: {type: DataTypes.STRING(50)},
     amount: {type: DataTypes.DECIMAL, allowNull: true},
     percent: {type: DataTypes.DECIMAL, allowNull: true},
     limit: {type: DataTypes.INTEGER, allowNull: true},
--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -4,17 +4,17 @@ const sequelize = require("../../configs/sequelize.config");
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
     fullname: {type: DataTypes.STRING, allowNull: true},
-    mobile: {type: DataTypes.STRING(15), allowNull: false, unique: true},
+    mobile: {type: DataTypes.STRING(15), allowNull: false},
     wallet_balance: {type: DataTypes.DECIMAL(10, 2), defaultValue: 0},
-    otpId: {type: DataTypes.INTEGER, allowNull: true, unique: true}
+    otpId: {type: DataTypes.INTEGER, allowNull: true}
 }, {timestamps: true, createdAt: "created_at", updatedAt: false});
 
 const Otp = sequelize.define("user_otp", {
     id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
     code: {type: DataTypes.STRING(5), allowNull: false},
     expires_in: {type: DataTypes.DATE, allowNull: false},
-    userId: {type: DataTypes.INTEGER, allowNull: false, unique: true}
-});
+    userId: {type: DataTypes.INTEGER, allowNull: false}
+}, {timestamps: false});
 module.exports = {
     User,
     Otp
